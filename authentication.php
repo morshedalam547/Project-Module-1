7e4cfe664ed626aa9e1c4868a1b0b55b769da3de
Morshed Alam <157200324+morshedalam547@users.noreply.github.com>
2025-03-16 19:17:00 +0600

    add

--- authentication.php
+++ authentication.php
@@ -9,5 +9,13 @@ $inputUsername = readline();
 echo "Enter password: ";
 $inputPassword =  readline();
 
+if($inputUsername === USERNAME && $inputPassword === PASSWORD){
+    echo "Login successful!\n";
+} else {
+    echo "Invalid username or password.\n";
+}
 
-echo ($inputUsername === USERNAME && $inputPassword === PASSWORD) ? "Login successful!\n" : "Invalid username or password.\n";
+
+//or you can use ternary operator
+
+// echo ($inputUsername === USERNAME && $inputPassword === PASSWORD) ? "Login successful!\n" : "Invalid username or password.\n";
